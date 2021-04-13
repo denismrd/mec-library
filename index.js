5b943a64d0f9f23f6840640d9b7fca1bdf038f66
Daniel mrd <denismrd93@gmail.com>
2021-04-13 17:54:26 +0530

    delivering static website

--- index.js
+++ index.js
@@ -4,5 +4,6 @@ const app = express()
 let port = process.env.PORT || 8000;
 app.listen(port, ()=>console.log("started"))
 app.get("/", (req, res)=> {
-    res.send("hello from server side")
+    res.sendFile(`${__dirname}/static/index.html`)
 })
+app.use(express.static(`${__dirname}/static`))
